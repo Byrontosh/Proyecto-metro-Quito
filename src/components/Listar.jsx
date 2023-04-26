495e2c6e5aeb5d8bef9f07b078a50dbaee1399dc
BYRON LOARTE <by_tosh20@hotmail.com>
2023-04-26 15:36:13 -0500

    Update Listar.jsx

--- src/components/Listar.jsx
+++ src/components/Listar.jsx
@@ -1,14 +1,14 @@
 import logoMetro from '../assets/tren.webp'
 import Mensajes from './Mensajes'
-import { useState, useEffect } from "react"
+import { useState } from "react"
+import { useEffect } from 'react'
 
-const Listar = ({estado}) => {
+const Listar = ({ estado,setIdmetro }) => {
+
     const [rutas, setRutas] = useState([])
 
-
     useEffect(() => {
-        if(estado)
-        {
+        if (estado || rutas.length >= 0) {
             (async function () {
                 try {
                     const respuesta = await (await fetch("http://localhost:3000/metro")).json()
@@ -39,8 +39,6 @@ const Listar = ({estado}) => {
         }
     }
 
-
-
     return (
         <>
             {
@@ -50,9 +48,7 @@ const Listar = ({estado}) => {
                     :
                     rutas.map((ruta) => (
                         <div key={ruta.id} className="p-2 rounded-xl sm:flex gap-12 bg-gray-200 shadow-xl mb-5">
-
                             <img src={logoMetro} alt="art cover" className="sm:w-3/12 object-cover rounded-lg" />
-
                             <div className="h-auto p-3 w-full">
                                 <h4 className="text-2xl font-semibold text-cyan-900">{ruta.nombre}</h4>
                                 <hr className="w-full border border-gray-300 my-2" />
@@ -62,19 +58,16 @@ const Listar = ({estado}) => {
                                 <p className="text-gray-500">Maquinista: {ruta.maquinista}</p>
                                 <p className="text-gray-500">Detalles: {ruta.detalles}</p>
                                 <div className='flex justify-between mt-3 lg:justify-end md:justify-end gap-3'>
-                                    <button className='bg-sky-900 text-white px-6 py-1 rounded-full'>Actualizar</button>
-
-                                    <button className='bg-red-900 text-white px-6 py-1 rounded-full'
-                                        onClick={()=>{handleDelete(ruta.id)}}
+                                    <button className='bg-sky-900 text-white px-6 py-1 rounded-full'
+                                        onClick={() => { setIdmetro(ruta.id) }}
+                                    >Actualizar</button>
+                                    <button className='bg-red-900 text-white px-6 py-1 rounded-full' onClick={() => { handleDelete(ruta.id) }}
                                     >Eliminar</button>
-
                                 </div>
                             </div>
                         </div>
                     ))
             }
-
-
         </>
 
     )
